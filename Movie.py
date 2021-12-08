0bb0c9a5009df274c1dd886be386a07f67f2114f
konstancjapradelska <59123568+konstancjapradelska@users.noreply.github.com>
2021-12-08 22:47:06 +0100

    Add files via upload

---
+++ Movie.py
@@ -0,0 +1,5 @@
+class Movie:
+    def __init__(self, movieID, title, genres):
+        self.movieID = movieID
+        self.title = title
+        self.genres = genres
---
+++ Rating.py
@@ -0,0 +1,6 @@
+class Rating:
+    def __init__(self, userID, movieID, rating, timestamp):
+        self.userID = userID
+        self.movieID = movieID
+        self.rating = rating
+        self.timestamp = timestamp
