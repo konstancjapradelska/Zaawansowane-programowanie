a6c9deee51c487aabbe2c9b6a428e9464664f275
konstancjapradelska <59123568+konstancjapradelska@users.noreply.github.com>
2021-12-08 22:49:02 +0100

    Delete lab_7 directory

--- lab_7/Tags.py
+++
@@ -1,6 +0,0 @@
-class Tags:
-    def __init__(self, userID, movieID, tag, timestamp):
-        self.userID = userID
-        self.movieID = movieID
-        self.tag = tag
-        self.timestamp = timestamp
