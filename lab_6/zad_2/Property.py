d970020fd524120e1e59b0650fb882eb6dc164da
konstancjapradelska <59123568+konstancjapradelska@users.noreply.github.com>
2022-01-18 15:24:38 +0100

    Delete lab_6 directory

--- lab_6/zad_2/Property.py
+++
@@ -1,7 +0,0 @@
-class Property:
-    def __init__(self, area, rooms, price, address):
-        self.area = area
-        self.rooms = rooms
-        self.price = price
-        self.address = address
-
